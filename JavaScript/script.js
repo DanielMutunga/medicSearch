160248a12360c05fc0bcdda87d42fcbc5b5fe3c1
Daniel Mutunga <mutungadaniel98@gmail.com>
2023-09-07 01:47:08 +0300

    added box shadow to the navlinks

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,6 +1,7 @@
-document.getElementById("services").onclick = function (){
-    window.location.href = "ASSETS/view.html"
-}
-document.getElementById("Services").onclick = function (){
-    window.location.href = "ASSETS/view.html"
-}
+// document.getElementById("services").onclick = function (){
+//     window.location.href = "ASSETS/view.html"
+// }
+// document.getElementById("Services").onclick = function (){
+//     window.location.href = "ASSETS/view.html"
+// }
+
