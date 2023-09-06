f49735c02c74a02cc0315b8aace83ed0973433e5
Daniel Mutunga <mutungadaniel98@gmail.com>
2023-09-07 00:02:48 +0300

    Initialised sign up form

---
+++ JavaScript/script.js
@@ -0,0 +1,6 @@
+document.getElementById("services").onclick = function (){
+    window.location.href = "ASSETS/view.html"
+}
+document.getElementById("Services").onclick = function (){
+    window.location.href = "ASSETS/view.html"
+}
