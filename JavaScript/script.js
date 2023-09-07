101fceaee69071234447b374559a14c3433c29b5
Daniel Mutunga <mutungadaniel98@gmail.com>
2023-09-07 15:06:20 +0300

    Added Links for navigation

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,7 +1,16 @@
-// document.getElementById("services").onclick = function (){
-//     window.location.href = "ASSETS/view.html"
-// }
-// document.getElementById("Services").onclick = function (){
-//     window.location.href = "ASSETS/view.html"
-// }
+document.getElementById("account").onclick = function (){
+    window.location.href = "ASSETS/signIn.html"
+}
+document.getElementById("services").onclick = function (){
+    window.location.href = "ASSETS/Services.html"
+}
+document.getElementById("seeMore").onclick = function (){
+    window.location.href = "ASSETS\Therapist.html"
+}
+document.getElementById("sign").onclick = function (){
+    window.location.href = "ASSETS/signIn.html"
+}
+document.getElementById("blog").onclick = function (){
+    window.location.href = "ASSETS/blog.html"
+}
 
